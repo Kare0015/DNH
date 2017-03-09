396f37a46abc45770011ddc5a434b43b08fdf373
Giorgio Joziasse <giorgio_giorgio@me.com>
2017-03-09 16:42:58 +0100

    added facturen page

---
+++ DNHAdminDashboard/resources/views/admin/facturen.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1>Facturen generen</h1>
+@stop
+
+@section('content')
+    <p>Selecteer hier de gebruikers om de facturen te genereren</p>
+@stop
