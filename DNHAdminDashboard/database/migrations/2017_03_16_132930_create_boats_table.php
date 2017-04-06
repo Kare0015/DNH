19f96074af58d6582ff85f5c2a28751ca6179d82
Giorgio Joziasse <giorgio_giorgio@me.com>
2017-04-06 15:00:38 +0200

    camelcase

--- DNHAdminDashboard/database/migrations/2017_03_16_132930_create_boats_table.php
+++ DNHAdminDashboard/database/migrations/2017_03_16_132930_create_boats_table.php
@@ -16,8 +16,8 @@ class CreateBoatsTable extends Migration
         Schema::create('boats', function (Blueprint $table) {
             $table->increments('id');
             $table->boolean('mainBoat');
-            $table->string('boatname');
-            $table->integer('boatlength');
+            $table->string('boatName');
+            $table->integer('boatLength');
             $table->timestamps();
         });
     }
